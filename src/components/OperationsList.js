0d17a850da7110cf1004a9008968681e93180794
Pismerova Victoria <pvikka@mail.ru>
2018-12-24 11:39:03 +0300

    Operations filter

--- src/components/OperationsList.js
+++ src/components/OperationsList.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactTable from "react-table";
 import Navigation from "./Navigation";
-// // import BootstrapTable, {TableHeaderColumn}  from "bootstrap";
-// // import $ from 'jquery';
-// import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
-//
+import 'react-month-picker/css/month-picker.css'
+import DatePicker from "react-datepicker/es";
+
 
 class OperationsList extends React.Component {
 
@@ -17,6 +16,7 @@ class OperationsList extends React.Component {
             selected: [],
             selectedNames: [],
             // selectAll: 0,
+            selectedDate: null,
             items: []
         };
         this.deleteHandleClick = this.deleteHandleClick.bind(this);
@@ -72,12 +72,26 @@ class OperationsList extends React.Component {
         }
         ];
 
-
         return (
             <div className="row">
+
                 <div className="col-sm-3  menu-style" style={{width: '100%'}}><Navigation/></div>
                 <div className="col-sm-15" style={{width: '75%'}}>
                     <h1>Operations List</h1>
+                    <input ref={input => (this._article = input)}
+                           id="article"
+                           name="article"
+                           required="true"
+                           placeholder="New article"/>
+                    <DatePicker id={"dateOperation"}
+                                dateFormat="yyyy-MM-dd"
+                                selected={this.state.selectedDate}
+                                onChange={(date) => this.setState({selectedDate: date})}
+                                placeholderText={"Select month"}
+                                dropdownMode={"select"}/>
+
+                    <button onClick={this.findHandleClick}>Find</button>
+                    <button onClick={this.clearHandleClick}>Clear</button>
                     <button onClick={this.deleteHandleClick}>Delete</button>
                     <ReactTable data={this.state.items}
                                 columns={columns}
@@ -86,6 +100,29 @@ class OperationsList extends React.Component {
                 </div>
             </div>
         );
+    }
+
+    findHandleClick = () => {
+        //if (this._article.value.length > 0 && this.state.selectedDate.value.length === 0) {
+        this.findArticle();
+        //}
+    };
+
+    findArticle() {
+        fetch("http://localhost:8080/operation/" + this.state.id + '/article/' + this._article.value, {
+            method: "GET",
+            dataType: "JSON",
+        })
+            .then((resp) => {
+                const copy = resp.json();
+                return copy;
+            })
+            .then((data) => {
+                this.setState({items: data});
+            })
+            .catch((error) => {
+                console.log(error, "catch the hoop")
+            });
     }
 
     onCheckboxChange(operationid) {
@@ -118,6 +155,11 @@ class OperationsList extends React.Component {
         console.log(this.selectedNames);
         window.location.reload();
 
+    };
+
+    clearHandleClick = () => {
+        document.getElementById("article").value = "";
+        document.getElementById("dateOperation").value = "";
     };
 
 
