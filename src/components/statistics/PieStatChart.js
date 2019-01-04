9c325b32e2f47a7f0e862b13c1f23470ce7697a3
Pismerova Victoria <pvikka@mail.ru>
2019-01-04 16:29:53 +0300

    PieChart statistics, need refactoring

--- src/components/statistics/PieStatChart.js
+++ src/components/statistics/PieStatChart.js
@@ -3,46 +3,63 @@ import PieChart from 'react-minimal-pie-chart';
 import 'react-minimal-pie-chart/dist/index'
 import MonthPickerInput from 'react-month-picker-input';
 import {Legend} from 'react-easy-chart'
-// import Picker from 'react-month-picker'
-// import 'react-month-picker/css/month-picker.css'
-// import "../style/style.css";
-// import 'react-month-picker/scss/month-picker.scss'
 import 'react-month-picker-input/dist/react-month-picker-input.css'
 
 export default class PieStatChart extends Component {
     constructor(props) {
         super(props);
-        this._selectedMonthYear = {};
+        this._selectedMonthYear = '12-2018';
+        this.state = {
+            id: localStorage.getItem('userSecretId'),
+            articles: [],
+            items: [],
+            data: []
+        }
+    }
+
+    componentDidMount() {
+        console.log("mount");
+        this.getData();
+    }
+
+    getData() {
+        console.log("http://localhost:8080/statistics/pie/" + this.state.id + "/" + this._selectedMonthYear);
+        fetch("http://localhost:8080/statistics/pie/" + this.state.id + "/" + this._selectedMonthYear, {
+            method: "GET",
+            dataType: "JSON",
+        })
+            .then((resp) => {
+                return resp.json();
+            })
+            .then((data) => {
+                this.setState({items: data});
+            })
+            .catch((error) => {
+                console.log(error, "catch the hoop")
+            });
+    }
+
+    buildPieData() {
+        this.state.data = [];
+        const config = [
+            {color: '#aaac84'},
+            {color: '#dce7c5'},
+            {color: '#e3a51a'}
+        ];
+        var i = 0;
+        this.state.items.forEach(value => {
+            var item = {
+                title: value.name,
+                value: value.value,
+                color: config[i].color
+            };
+            this.state.data.push(item);
+            i = i + 1;
+        });
+        this.state.data.forEach(value => console.log(value.title + ' ' + value.value + ' ' + value.color));
     }
 
     render() {
-        var data = [
-            {
-                title: 'One',
-                text: "TTT",
-                value: 10,
-                tag: 'ddd',
-                label: 'ffff',
-                name: 'name',
-                key: '123',
-                color: '#E38627'
-            },
-            {title: 'Two', value: 15, color: '#C13C37'},
-            {title: 'Three', value: 20, color: '#6A2135'},
-        ];
-
-        let pickerLang = {
-            months: ['Jan', 'Feb', 'Mar', 'Spr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-            , from: 'From', to: 'To'
-        }
-            , mvalue = {year: 2015, month: 11}
-            , mrange = {from: {year: 2014, month: 8}, to: {year: 2015, month: 5}};
-
-        let makeText = m => {
-            if (m && m.year && m.month) return (pickerLang.months[m.month - 1] + '. ' + m.year);
-            return '?'
-        };
-
         const config = [
             {color: '#aaac84'},
             {color: '#dce7c5'},
@@ -58,7 +75,7 @@ export default class PieStatChart extends Component {
                 padding: '12px'
             }
         };
-
+        this.buildPieData();
         return (
 
             <div>
@@ -68,10 +85,11 @@ export default class PieStatChart extends Component {
                 <div style={{width: '400px'}}>
                     <MonthPickerInput
                         year={2018}
-                        month={8}
+                        month={11}
                         onChange={(item, i, k) => {
                             console.log(item + ' ' + i + ' ' + k);
-                            this._selectedMonthYear = k + '-' + i;
+                            this._selectedMonthYear = (k + 1) + '-' + i;
+                            this.getData();
                         }}
                     />
                 </div>
@@ -80,19 +98,10 @@ export default class PieStatChart extends Component {
                 <div className="App">
                     <h1>My First LineChart</h1>
                     <div style={{width: '150px'}}>
-                        <PieChart
-
-                            labels
-                            data={data}
-                        />
 
                         <PieChart
                             labels={true}
-                            data={[
-                                {key: 'A', value: 100, color: '#000000'},
-                                {key: 'B', value: 200, color: '#ff77ff'},
-                                {key: 'C', value: 50, color: '#e3a51a'}
-                            ]}
+                            data={this.state.data}
                             styles={{
                                 '.chart_text': {
                                     fontSize: '14px',
@@ -100,7 +109,7 @@ export default class PieStatChart extends Component {
                                 }
                             }}
                         />
-                        <Legend data={data}
+                        <Legend data={this.state.data}
                                 dataId={'title'}
                                 config={config}
                                 styles={customStyle}
